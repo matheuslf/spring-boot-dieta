0899b6dfd6610eca5704f69f0bae97274482d6ac
Matheus <matheusleandroferreira@gmail.com>
2024-10-09 11:23:08 -0300

    Exclude DataSourceAutoConfiguration

--- src/main/java/spring/boot/dieta/DietaApplication.java
+++ src/main/java/spring/boot/dieta/DietaApplication.java
@@ -2,8 +2,9 @@ package spring.boot.dieta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class DietaApplication {
 
 	public static void main(String[] args) {
